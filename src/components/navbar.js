e01379dc76432e4dfffec61449bcd9bbf52dba15
Creighton Soukup <creighton@creightonsoukup.com>
2017-08-13 23:01:48 -0400

    navbar greater than tablet less than desktop"

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,27 +6,43 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 const Navbar = (props) => {
   const navBarItems = ['portolio', 'contact', 'blog', 'resume', 'client login']
+  const vpWidth = window.innerWidth
   return (
     <nav className='navbar'>
       <Logo/>
-      <div className='navlinks'>
-        { props.openNav ? (
-          <ul className='dropdown' onClick={props.toggleNav}>
-            <i className="fa fa-times"></i>
+      { vpWidth > 768 ? (
+        <div className='open'>
+          <ul className='left'>
             <li><Link to='/#portfolio'>portfolio</Link></li>
             <li><Link to='/#feed'>feed</Link></li>
             <li><Link to='/#blog'>blog</Link></li>
             <li><Link to='/#contact'>contact</Link></li>
+          </ul>
+          <ul className='right'>
             <li><a href={require('../assets/Soukup_Resume.pdf')} download>download resume</a></li>
-            <li>-</li>
             <li><Link to='/login'>client login</Link></li>
           </ul>
-        ) : (
-          <div onClick={props.toggleNav}>
-            <i className="fa fa-bars-btb"></i>
-          </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <div className='collapse'>
+          { props.openNav ? (
+            <ul className='dropdown' onClick={props.toggleNav}>
+              <i className="fa fa-times"></i>
+              <li><Link to='/#portfolio'>portfolio</Link></li>
+              <li><Link to='/#feed'>feed</Link></li>
+              <li><Link to='/#blog'>blog</Link></li>
+              <li><Link to='/#contact'>contact</Link></li>
+              <li><a href={require('../assets/Soukup_Resume.pdf')} download>download resume</a></li>
+              <li>-</li>
+              <li><Link to='/login'>client login</Link></li>
+            </ul>
+          ) : (
+            <div onClick={props.toggleNav}>
+              <i className="fa fa-bars-btb"></i>
+            </div>
+          )}
+        </div>
+      )}
     </nav>
   )
 }
